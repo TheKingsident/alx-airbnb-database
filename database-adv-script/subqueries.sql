b1655cb907f8d8e39fe545a7b6f6043f6e1bb942
TheKingsident <kingsident@gmail.com>
2025-05-14 20:10:01 +0100

    Refactor subqueries for clarity and consistency in property and user selection

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,6 +1,19 @@
+-- This query selects all properties from the Property table where the property_id
+-- matches the id of properties that have at least one review with a rating greater than 4.
 SELECT * FROM Property
 WHERE property_id IN (
     SELECT property.id
     FROM Review
     WHERE rating > 4
 );
+
+
+-- This query selects the id, first name, and last name of users
+-- who have made more than 3 bookings.
+SELECT User.id, User.first_name, User.last_name
+FROM User
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.user_id = User.id
+) > 3;
